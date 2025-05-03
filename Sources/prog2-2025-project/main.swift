d8e9996e7209bcccd75b42c3339cf6c28d4de8c4
Thomas GOFFIN <thomas.goffin.001@student.uni.lu>
2025-05-03 21:28:03 +0200

    trFait

--- Sources/prog2-2025-project/main.swift
+++ Sources/prog2-2025-project/main.swift
@@ -1,26 +1,3 @@
-
-// Affiche la map initiale
-//stageMTN.displayMap()
-
-// Exemples de déplacements
-//stageMTN.move(direction: "left")
-//stageMTN.move(direction: "up")
-// appel du menu
-//menu()
-
-//var rep = connecTo(stageActuel: stageMTN, stageSuivant: ForestStage)
-//print(rep)
-
-//changeStage(from: stageMTN, to: ForestStage)
-//stageMTN.displayMap()
-
-//changeStage(from: stageMTN, to: DungeonEntryStage)
-//stageMTN.displayMap()
-//askMove()
-//changeStage(from: stageMTN, to: ForestStage)
-//askStage()
 while !finished {
    menu()
-   
-
-}
+   }
