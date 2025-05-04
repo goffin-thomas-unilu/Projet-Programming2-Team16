31630b49c7083a706a2cae9c54ace58f90fa5d1e
Thomas GOFFIN <thomas.goffin.001@student.uni.lu>
2025-05-05 00:05:47 +0200

    in

--- Sources/prog2-2025-project/Command.swift
+++ Sources/prog2-2025-project/Command.swift
@@ -23,7 +23,7 @@ func printCommand(){
             // Show inventory
             // function to print inventory
             print("You have in your inventory :")
-            print(player.inventory.items)
+            player.inventory.display()
             menu()
         case "status":
             // Show health point, mana point + stats
